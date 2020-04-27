3c06adcd86faaa87a1eb22a5355fde732efa37bc
Babeesh Cherukutty <bab>
2020-04-27 15:39:18 +0530

    Added the weight calculation algorithm

--- perceptronAlgorithm.py
+++ perceptronAlgorithm.py
@@ -1,37 +1,87 @@
 
-# Making a prediction with weight calculation
-def prediction(row, weights, bias):
+# Estimating the values for the weight
+def weight_training(trainSet, learningRate, epoch):
     """
-    activation = sum(weight + training data) + bias
+    Learning rate: Weight correction rate during updation
+    Epochs: Number of execution of weight updation
 
+    w1 = w1 + Learning rate * (expected - predicted) * X1
+    bias = bias + Learning rate * (expected - predicted)
+
+    :return:
+    """
+    weight = [0.0 for i in range(len(trainSet[0]))]
+
+    for x in range(epoch):
+        total_error = 0.0
+        for row in trainSet:
+            # Predicting the value with existing variables
+            predicted_value = prediction(row, weight)
+
+            # Calculating the correctness of prediction
+            error = row[-1] - predicted_value
+            total_error += error**2
+
+            # Calculating the value for bias
+            weight[0] = weight[0] + learningRate * error
+            
+            #print(x, row, 'Expected:', row[-1], 'Predicted:', predicted_value, 'Error:', error, 'Total Error:', total_error)
+
+            # Weight for each input attribute
+            for i in range(len(row)-1):
+                weight[i + 1] = weight[i + 1] + learningRate * error * row[i]
+
+                #print(i, weight[i + 1], learningRate , error , row[i], end='')
+                #print('')
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, learningRate, total_error))
+    return weight
+
+# Making a prediction with weight calculation
+def prediction(row, weight):
+    """
+        activation = sum(weight + training data instances) + bias
+    when n=2
+        activation = (X1 * w1) + (X2 * w2) + bias
+
+    Weight values received through function
+    weight[0] = bias
+    weight[1] = w1
+    weight[n] = wn
+
+    :return:
     1.0 => activation >= 0.0
     0.0 => activation < 0.0
     """
 
-    sum = bias
+    sum = weight[0]
 
     for i in range(len(row) - 1):
-        #print(i, row[i], activation, weights[i + 1])
-        sum += weights[i + 1] * row[i]
-    print(sum)
+        #print(row[i])
+        sum += weight[i+1] * row[i]
+
     return 1.0 if sum >= 0.0 else 0.0
 
 
-# test predictions
-dataset = [[2.7810836, 2.550537003, 0]#,
-          # [1.465489372, 2.362125076, 0],
-          # [3.396561688, 4.400293529, 0],
-           #[1.38807019, 1.850220317, 0],
-           #[3.06407232, 3.005305973, 0],
-           #[7.627531214, 2.759262235, 1],
-           #[5.332441248, 2.088626775, 1],
-           #[6.922596716, 1.77106367, 1],
-           #[8.675418651, -0.242068655, 1],
-           #[7.673756466, 3.508563011, 1]
+# Test data
+dataset = [[2.7810836, 2.550537003, 0],
+           [1.465489372, 2.362125076, 0],
+           [3.396561688, 4.400293529, 0],
+           [1.38807019, 1.850220317, 0],
+           [3.06407232, 3.005305973, 0],
+           [7.627531214, 2.759262235, 1],
+           [5.332441248, 2.088626775, 1],
+           [6.922596716, 1.77106367, 1],
+           [8.675418651, -0.242068655, 1],
+           [7.673756466, 3.508563011, 1]
            ]
-weights = [-0.1, 0.20653640140000007, -0.23418117710000003]
-bias = -0.1
+# Weight
+learningRate = 0.1
+epoch = 5
+weight = weight_training(dataset, learningRate, epoch)
+
+print(weight)
 
 for row in dataset:
-    predicted = prediction(row, weights, bias)
-    #print("Expected=%d, Predicted=%d" % (row[-1], predicted))
+    pass
+    #predicted_value = prediction(row, weights)
+    #print("Expected=%d, Predicted=%d" % (row[-1], predicted_value))
